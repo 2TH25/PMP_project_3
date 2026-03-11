7d74bc954ee9fd707aa43c311e525719c9b03566
tvie <thomas.vie@edu.umlp.fr>
2026-03-11 10:16:38 +0100

    Add combiners

--- Signal.h
+++ Signal.h
@@ -1,52 +1,57 @@
 #ifndef SIGNAL_H
 #define SIGNAL_H
 
+#include <functional>
 #include <optional>
+#include <vector>
 
 namespace sig {
 
   class DiscardCombiner {
   public:
-    using result_type = /* implementation defined */;
+    using result_type = void;
 
     template<typename U>
-    void combine(/* implementation defined */ item) {
-      // implementation defined
-    }
+    void combine(U item) {}
 
-    result_type result() {
-      // implementation defined
-    }
+    result_type result() {}
   };
 
   template<typename T>
   class LastCombiner {
   public:
-    using result_type = /* implementation defined */;
+    using result_type = T;
 
     template<typename U>
-    void combine(/* implementation defined */ item) {
-      // implementation defined
+    void combine(U item) {
+      res = item;
     }
 
     result_type result() {
-      // implementation defined
+      return res;
     }
+
+  private:
+    result_type res;
   };
 
   template<typename T>
   class VectorCombiner {
   public:
-    using result_type = /* implementation defined */;
+    using result_type = std::vector<T>;
+
+    VectorCombiner(): res() {}
 
     template<typename U>
-    void combine(/* implementation defined */ item) {
-      // implementation defined
+    void combine(U item) {
+      res.emplace_back(item);
     }
 
     result_type result() {
-      // implementation defined
+      return res;
     }
+  private:
+    result_type res;
   };
 
   enum class PredicateType {
@@ -57,20 +62,30 @@ namespace sig {
   template<typename T, PredicateType PType = PredicateType::Binary>
   class PredicateCombiner {
   public:
-    using result_type = std::optional</* implementation defined */>;
+    using result_type = std::optional<T>;
+    using predicate_type = std::conditional_t<PType == PredicateType::Unary, std::function<bool(T)>, std::function<bool(T, T)>>;
 
-    PredicateCombiner(/* implementation defined */ predicate) {
-      // implementation defined
-    }
+    PredicateCombiner(std::function<predicate_type> predicate): predicate(predicate) {}
 
     template<typename U>
-    void combine(/* implementation defined */ item) {
-      // implementation defined
+    void combine(U item) {
+      if constexpr (PType == PredicateType::Binary) {
+        if (!res.has_value()) {
+          res = item;
+        } else if (predicate(*res, item)) {
+          res = item;
+        }
+      } else {
+        if (predicate(item)) res = item;
+      }
     }
 
     result_type result() {
-      // implementation defined
+      return res;
     }
+  private:
+    predicate_type predicate;
+    result_type res;
   };
 
   template<typename Signature, typename Combiner = DiscardCombiner>

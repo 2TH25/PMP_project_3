0c2dbe17df9ea653dfa0af00d7cb897e8336e975
mgirar10 <mattheo.girardclos@edu.univ-fcomte.fr>
2026-03-11 09:18:21 +0000

    add class Signal

--- Signal.h
+++ Signal.h
@@ -4,10 +4,14 @@
 #include <functional>
 #include <optional>
 #include <vector>
+#include <utility>
+#include <map>
 
-namespace sig {
+namespace sig
+{
 
-  class DiscardCombiner {
+  class DiscardCombiner
+  {
   public:
     using result_type = void;
 
@@ -17,8 +21,9 @@ namespace sig {
     result_type result() {}
   };
 
-  template<typename T>
-  class LastCombiner {
+  template <typename T>
+  class LastCombiner
+  {
   public:
     using result_type = T;
 
@@ -35,8 +40,9 @@ namespace sig {
     result_type res;
   };
 
-  template<typename T>
-  class VectorCombiner {
+  template <typename T>
+  class VectorCombiner
+  {
   public:
     using result_type = std::vector<T>;
 
@@ -54,13 +60,15 @@ namespace sig {
     result_type res;
   };
 
-  enum class PredicateType {
+  enum class PredicateType
+  {
     Unary,
     Binary,
   };
 
-  template<typename T, PredicateType PType = PredicateType::Binary>
-  class PredicateCombiner {
+  template <typename T, PredicateType PType = PredicateType::Binary>
+  class PredicateCombiner
+  {
   public:
     using result_type = std::optional<T>;
     using predicate_type = std::conditional_t<PType == PredicateType::Unary, std::function<bool(T)>, std::function<bool(T, T)>>;
@@ -88,32 +96,49 @@ namespace sig {
     result_type res;
   };
 
-  template<typename Signature, typename Combiner = DiscardCombiner>
-  class Signal {
+  template <typename Signature, typename Combiner = DiscardCombiner>
+  class Signal
+  {
+
+    Combiner m_combiner;
+    std::map<std::size_t, std::function<Signature>> m_functions;
+
   public:
-    using combiner_type = /* implementation defined */;
+    using combiner_type = Combiner;
 
-    using result_type = /* implementation defined */;
+    using result_type = Combiner::result_type;
 
-    Signal(Combiner combiner = Combiner()) {
-      // implementation defined
+    Signal(Combiner combiner = Combiner())
+        : m_combiner(combiner) {}
+
+    template <typename... CombinerArgs>
+    Signal(CombinerArgs... args)
+        : m_combiner(args) {}
+
+    std::size_t connectSlot(std::function<Signature> callback)
+    {
+      bool is_add(false);
+      std::size_t id(0);
+      do
+      {
+        is_add = m_functions.insert({id, callback}).second;
+        id++;
+      } while (!is_add);
+      return id - 1;
     }
 
-    template<typename... CombinerArgs>
-    Signal(CombinerArgs ... args) {
-      // implementation defined
+    void disconnectSlot(std::size_t id)
+    {
+      m_functions.erase(id);
     }
 
-    std::size_t connectSlot(std::function<Signature> callback) {
-      // implementation defined
-    }
+    template <typename... Args>
+    result_type emitSignal(Args)
+    {
+      for (auto fun : m_functions)
+        m_combiner.combine(m_functions(Args));
 
-    void disconnectSlot(std::size_t id) {
-      // implementation defined
-    }
-
-    result_type emitSignal(/* implementation defined */) {
-      // implementation defined
+      return m_combiner.result();
     }
   };
 
